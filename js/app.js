01319f398777351658c96b22b261f39cecafdf1b
DevYoma <Lawrenceyoma@gmail.com>
2020-06-18 21:08:19 +0100

    about me $ social media links

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function myWhatsapp(){
+    location.href = "https://wa.link/cojs69";
+}
+
+function myFaceBook(){
+    location.href = "https://web.facebook.com/settings?tab=account&section=username";
+}
+
+function myTwitter(){
+    location.href = "https://twitter.com/devYoma";
+}
